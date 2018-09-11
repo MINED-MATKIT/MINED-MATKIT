b80d7c2ba02b4f8561fa5607fc452031739a89ce
Ahmet Cecen <ahmetcecen@gmail.com>
2018-09-10 23:26:54 -0500

    Dump Archive - Cleaning Required

---
+++ RemovePathScript.m
@@ -0,0 +1,10 @@
+rmpath C:\Users\Silver\Documents\MATLAB\sliceomatic
+rmpath C:\Users\Silver\Documents\MATLAB\sliceomatic\sliceomatic-1.0
+rmpath C:\Users\Silver\Documents\GitHub\IsoSlicer-MATLAB
+rmpath C:\Users\Silver\Documents\GitHub\MultiPolyRegress-MatlabCentral
+rmpath C:\Users\Silver\Documents\GitHub\ShadowRemover-MatlabCentral
+rmpath C:\Users\Silver\Documents\GitHub\MATLAB-Spatial-Correlation-Toolbox
+rmpath C:\Users\Silver\Documents\MATLAB\Toolboxes
+
+rmpath C:\Users\Silver\Documents\MATLAB\matconvnet-1.0-beta16
+rmpath C:\Users\Silver\Documents\MATLAB\matconvnet-1.0-beta16\matlab
